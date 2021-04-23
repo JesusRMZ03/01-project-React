311fdcfd86df16d6d0b40452e6a537b245675084
JesusRMZ03 <A01022447@itesm.mx>
2021-04-22 22:35:05 -0500

    Cambiando el tema

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './DDSF.png';
 import './App.css';
 
 function App() {
@@ -6,9 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://dojodevs.slack.com/archives/C01H9UV7N07"
@@ -17,7 +19,9 @@ function App() {
         >
           Dedicado para DojoDevs
         </a>
+
         <p>Aqui va a estar mi aplicacion, en la cual voy aprender a subir archivos para mi escuela</p>
+
       </header>
     </div>
   );
