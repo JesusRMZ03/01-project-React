f3531dd15edc0ffd8e79774c8050e8fc8ecb5a50
JesusRMZ03 <A01022447@itesm.mx>
2021-04-26 22:41:25 -0500

    Iniciando Proyecto

--- src/App.js
+++ src/App.js
@@ -4,25 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <a
-          className="App-link"
-          href="https://dojodevs.slack.com/archives/C01H9UV7N07"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Dedicado para DojoDevs
-        </a>
-
-        <p>Aqui va a estar mi aplicacion, en la cual voy aprender a subir archivos para mi escuela</p>
-
-      </header>
+      <div className="Content">
+        
+      </div>
     </div>
   );
 }
